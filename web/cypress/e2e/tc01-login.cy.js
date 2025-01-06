04b14718f3316e66da152691d672128976446e9d
Juliano Peres <juliano.a.peres@gmail.com>
2025-01-06 16:57:48 -0300

    Readme with orientations about the environment set up; tc01 with the sccess login scenarios

--- web/cypress/e2e/tc01-login.cy.js
+++ web/cypress/e2e/tc01-login.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Login", () => {
+  it('It should login successfully when the user fill out the form with valid credentials by clicking on the "Entrar" button', () => {
+    cy.visit("/"); // Given the user access this link
+    cy.get("a[href = 'entrar']").click(); // When the user clicks on the "Entrar" button
+    cy.get("h2").should("have.text", "Seus dados"); // Then the user should see the "Seus dados" title
+    cy.get("input[placeholder='Nome']").type("Teste Qa"); // And fill out the "name" field with "Teste Qa"
+    cy.get("input[placeholder='E-mail']").type("qateste@teste.com"); // And fill out the "email" field
+    cy.get("input[placeholder='Whatsapp']").type("14345678912"); // And fill out the "password" field
+    cy.get("button[type='submit']").click(); // And click on the "Continuar" button
+    cy.get("h1").should("have.text", "O LendárioBarbershop"); // Then the user should see the "O LendárioBarbershop" title
+  });
+  it('It should login successfully when the user fill out the form with valid credentials by clicking on the "Agendar um horário" button', () => {
+    cy.visit("/"); // Given the user access this link
+    cy.get("a[href = '/agendamento']").click(); // When the user clicks on the "Entrar" button
+    cy.get("h2").should("have.text", "Seus dados"); // Then the user should see the "Seus dados" title
+    cy.get("input[placeholder='Nome']").type("Teste Qa"); // And fill out the "name" field with "Teste Qa"
+    cy.get("input[placeholder='E-mail']").type("qateste@teste.com"); // And fill out the "email" field
+    cy.get("input[placeholder='Whatsapp']").type("14345678912"); // And fill out the "password" field
+    cy.get("button[type='submit']").click(); // And click on the "Continuar" button
+    cy.get("h1").should("have.text", "Agendamento de Serviços"); // Then the user should see the "Agendamento de Serviços" title
+  });
+});
