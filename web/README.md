8c1bc3476a69a2652987e10fbe5529d80e24a969
Juliano Peres <juliano.a.peres@gmail.com>
2025-01-11 17:04:01 -0300

    Big package done

--- web/README.md
+++ web/README.md
@@ -12,4 +12,3 @@ PROJECT DIRECTORY PREPARATION
 Install Yarn: After instal, open the Command Prompt as Administrator and run the command: corepack enable. This will activate Corepack and enable Yarn. This step is only necessary the first time.
 Install Cypress 13.15.2
 Install: cypress-xpath
-
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -23,15 +23,59 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-// ***********************************************
-Cypress.Commands.add("assertValidationMessage", (selector, expectedMessage) => {
-  cy.get(selector).then(($input) => {
-    const actualMessage = $input[0].validationMessage;
-    cy.log(`Validation message for ${selector}: ${actualMessage}`);
-    console.log(`Validation message for ${selector}: ${actualMessage}`);
-    if (actualMessage !== expectedMessage) {
-      cy.log(`Expected: ${expectedMessage}, but got: ${actualMessage}`);
-    }
-    expect(actualMessage).to.eq(expectedMessage);
-  });
+
+// ***** ALLOW CYPRESS RUN THE TESTS EVEN WITH ERROR *****
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from failing the test
+  return false;
 });
+
+//! *|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*
+
+// ***** TEST COUNTER *****
+
+// Initialize a global counter for describe blocks
+let globalDescribeCounter = 0;
+
+/**
+ * Custom function to create a numbered `describe` block.
+ * Automatically enumerates `describe` and nested `it` blocks.
+ *
+ * @param {string} description - The description for the `describe` block.
+ * @param {function} callback - A callback function containing the `it` blocks.
+ */
+const customDescribeNumbered = (description, callback) => {
+  // Increment the global describe counter for each new block
+  const currentDescribeCounter = ++globalDescribeCounter;
+
+  // Initialize an it-block counter specific to this describe block
+  let localItCounter = 0;
+
+  /**
+   * Custom function to create a numbered `it` block within the describe block.
+   *
+   * @param {string} itDescription - The description for the `it` block.
+   * @param {function} itCallback - The callback function for the test.
+   */
+  const itWithNumbering = (itDescription, itCallback) => {
+    // Increment the it-block counter for each new `it`
+    localItCounter++;
+    const numberedItDescription = `${currentDescribeCounter}.${localItCounter} ${itDescription}`;
+    it(numberedItDescription, itCallback);
+  };
+
+  // Add numbering to the `describe` block
+  const numberedDescription = `${currentDescribeCounter}. ${description}`;
+
+  // Execute the `describe` block with the numbered description
+  describe(numberedDescription, () => {
+    callback(itWithNumbering);
+  });
+};
+
+// Export the custom describeNumbering function for use in tests
+module.exports = {
+  customDescribeNumbered,
+};
+
+//! *|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*
